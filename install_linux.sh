80de6d378910c00a935b065f11d8ed4c343e14fa
Chul-Woong Yang <cwyang@gmail.com>
2020-12-01 23:20:00 +0900

    use `sh`
    not `bash`

--- install_linux.sh
+++ install_linux.sh
@@ -1,46 +1,59 @@
 #!/bin/sh
 set -e
-
-CERTFILE=foo.pem
-CERTNAME="Local Root CA"
-
-if ! type update-ca-certificates > /dev/null; then
+install_certs() {
+    $cert_path=$certLocation/$CERTFILE
+    
+    if ! type update-ca-certificates > /dev/null; then
 	echo "This system does not have 'update-ca-certificates' command."
-	echo "Please install the certificate manually."
+        >	echo "Please install the certificate manually."
 	exit 1
-fi
-if ! type certutil > /dev/null; then
+    fi
+    if ! type certutil > /dev/null; then
 	echo "This system does not have 'certutil' command."
 	echo "Please install the parent package like 'libnss3-tools' or so,"
 	echo "Otherwise, please install the certificate manually."
 	exit 1
-fi
+    fi
 
-echo "Installing certificate to system certificate repository.."
-sudo mkdir -p /usr/local/share/ca-certificates/extra
-sudo cp $CERTFILE /usr/local/share/ca-certificates/extra/$CERTFILE
-sudo update-ca-certificates
+    echo "Installing certificate to system certificate repository.."
+    sudo mkdir -p /usr/local/share/ca-certificates/extra
+    sudo cp $CERTFILE /usr/local/share/ca-certificates/extra/$CERTFILE
+    sudo update-ca-certificates
 
-###
-### For cert8 (legacy - DBM)
-###
+    ###
+    ### For cert8 (legacy - DBM)
+    ###
 
-echo "Updating local user cert8 repository.."
-for certDB in $(find ~/ -name "cert8.db")
-do
-    certdir=$(dirname ${certDB});
-    certutil -A -n "${CERTNAME}" -t "TCu,Cu,Tu" -i ${CERTFILE} -d dbm:${certdir}
-done
+    echo "Updating local user cert8 repository.."
+    for certDB in $(find ~/ -name "cert8.db")
+    do
+        certdir=$(dirname ${certDB});
+        certutil -A -n "${CERTNAME}" -t "TCu,Cu,Tu" -i ${cert_path} -d dbm:${certdir}
+    done
 
 
-###
-### For cert9 (SQL)
-###
+    ###
+    ### For cert9 (SQL)
+    ###
 
-echo "Updating local user cert9 repository.."
-for certDB in $(find ~/ -name "cert9.db")
-do
-    certdir=$(dirname ${certDB});
-    certutil -A -n "${CERTNAME}" -t "TCu,Cu,Tu" -i ${CERTFILE} -d sql:${certdir}
-done
-echo "Done."
+    echo "Updating local user cert9 repository.."
+    for certDB in $(find ~/ -name "cert9.db")
+    do
+        certdir=$(dirname ${certDB});
+        certutil -A -n "${CERTNAME}" -t "TCu,Cu,Tu" -i ${cert-Path} -d sql:${certdir}
+    done
+
+    echo "Done."
+}
+
+CERTFILE=foo.pem
+CERTNAME="Local Root CA"
+
+certLocation="/tmp/pki/"
+mkdir -p $certLocation
+rootCerts=$CERTFILE
+cat <<EOF | base64 -d > $certLocation/$CERTFILE
+MIID/zCCAuegAwIBAgIUXNjylyR7JNg4ZdbIgkba2rEU/tUwDQYJKoZIhvcNAQEL
+...
+EOF
+install_cert
--- install_mac.command
+++ install_mac.command
@@ -1,23 +1,23 @@
 #!/bin/sh
-function install_cert() {
-	for cert in "${rootCerts[@]}"
-	do
-	    cmd="security add-trusted-cert -d -k /Library/Keychains/System.keychain -r trustRoot $certLocation$cert"
-	    prompt='Installing an SSL certificate'
-	    OUTPUT=$(osascript -e "do shell script \"${cmd}\" with administrator privileges with prompt \"${prompt}\"" 2>&1)
-	    if [ $? -ne 0 ]; then
-		osascript -e "display dialog \"${OUTPUT}\" with title \"Installation Failed\" buttons{\"OK\"} default button \"OK\"" >& /dev/null
-		exit 1
-	    else
-		osascript -e "display dialog \"The certificate has been installed to System Keychain.\" with title \"Installation completed\" buttons {\"OK\"} default button \"OK\"" >& /dev/null
-	    fi
-	done
+install_cert() {
+    cmd="security add-trusted-cert -d -k /Library/Keychains/System.keychain -r trustRoot $certLocation$cert"
+    prompt='Installing an SSL certificate'
+    OUTPUT=$(osascript -e "do shell script \"${cmd}\" with administrator privileges with prompt \"${prompt}\"" 2>&1)
+    if [ $? -ne 0 ]; then
+	osascript -e "display dialog \"${OUTPUT}\" with title \"Installation Failed\" buttons{\"OK\"} default button \"OK\"" >& /dev/null
+	exit 1
+    else
+	osascript -e "display dialog \"The certificate has been installed to System Keychain.\" with title \"Installation completed\" buttons {\"OK\"} default button \"OK\"" >& /dev/null
+    fi
 }
+
+CERTFILE=foo.pem
+CERTNAME="Local Root CA"
 
 certLocation="/tmp/pki/"
 mkdir -p $certLocation
-declare -a rootCerts=(A.cert);
-cat <<EOF | base64 -D > $certLocation/A.cert
+rootCerts=$CERTFILE
+cat <<EOF | base64 -D > $certLocation/$CERTFILE
 MIID/zCCAuegAwIBAgIUXNjylyR7JNg4ZdbIgkba2rEU/tUwDQYJKoZIhvcNAQEL
 ...
 EOF
