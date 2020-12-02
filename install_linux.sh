0be0aed44ea8bbc5663075aef847b5f71473c76d
Chul-Woong Yang <cwyang@gmail.com>
2020-12-02 15:03:28 +0900

    GUI (zenity) & downloading cert from given URL

--- install_linux.sh
+++ install_linux.sh
@@ -1,30 +1,48 @@
-#!/bin/sh
-set -e
-install_certs() {
-    $cert_path=$certLocation/$CERTFILE
-    
-    if ! type update-ca-certificates > /dev/null; then
-	echo "This system does not have 'update-ca-certificates' command."
-        >	echo "Please install the certificate manually."
+#!/bin/bash
+export SUDO_ASKPASS=`which /usr/bin/ssh-askpass`
+gui=1
+require_program() {
+    if ! type $1 >& /dev/null; then
+	echo "This system does not have '$1' command."
+	echo "Please install the certificate '$cert_path' manually."
 	exit 1
     fi
-    if ! type certutil > /dev/null; then
-	echo "This system does not have 'certutil' command."
-	echo "Please install the parent package like 'libnss3-tools' or so,"
-	echo "Otherwise, please install the certificate manually."
-	exit 1
+}
+gui_check() {
+    if ! type zenity > /dev/null; then
+        gui=0
+    elif ! type $SUDO_ASKPASS > /dev/null; then
+        gui=0
+    elif [ x"$DISPLAY" == x ]; then
+        gui=0
     fi
-
-    echo "Installing certificate to system certificate repository.."
-    sudo mkdir -p /usr/local/share/ca-certificates/extra
-    sudo cp $CERTFILE /usr/local/share/ca-certificates/extra/$CERTFILE
-    sudo update-ca-certificates
+}
+mesg() {
+    if [ $gui -eq 0 ]; then
+        echo $2
+    else
+        echo "$1"; sleep 1
+        echo "# $2"
+    fi
+}
+info() {
+    if [ $gui -eq 0 ]; then
+        echo $1
+    else
+        zenity --info --text="$1" --width=400
+    fi
+}
+install_main() {
+    mesg 10 "Installing certificate to system certificate repository.."
+    sudo -A mkdir -p /usr/local/share/ca-certificates/extra
+    sudo -A cp $cert_path /usr/local/share/ca-certificates/extra/$CERTFILE
+    sudo -A update-ca-certificates
 
     ###
     ### For cert8 (legacy - DBM)
     ###
 
-    echo "Updating local user cert8 repository.."
+    mesg 30 "Updating local user cert8 repository.."
     for certDB in $(find ~/ -name "cert8.db")
     do
         certdir=$(dirname ${certDB});
@@ -36,24 +54,66 @@ install_certs() {
     ### For cert9 (SQL)
     ###
 
-    echo "Updating local user cert9 repository.."
+    mesg 60 "Updating local user cert9 repository.."
     for certDB in $(find ~/ -name "cert9.db")
     do
         certdir=$(dirname ${certDB});
         certutil -A -n "${CERTNAME}" -t "TCu,Cu,Tu" -i ${cert-Path} -d sql:${certdir}
     done
 
-    echo "Done."
+    mesg 99 "Done."
+}
+install_cert() {
+
+    require_program update-ca-certificates
+    require_program openssl
+    require_program certutil
+
+    subj=`openssl x509 -in $cert_path -noout -text -inform DER| perl -ne 'print $1 if /Subject: (.*)/'`
+    if [ $gui -eq 0 ]; then
+        echo "This program installs CA certificate \"$subj\"."
+        install_main
+    else
+        zenity --question --text="This program installs CA certificate \"$subj\"." --width=400
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+        install_main | zenity --progress \
+                              --title="Installing Certificate" \
+                              --text="Preparing..." \
+                              --percentage=0 --width=400
+        if [ "$?" = -1 ] ; then
+            zenity --error \
+                   --text="Update canceled."
+        fi
+    fi
 }
 
-CERTFILE=foo.pem
+gui_check
+
+if ! type curl >& /dev/null; then
+    info "This system does not have 'curl' command."
+    info "Please install the certificate manually."
+    exit 1
+fi
+
+CERTURL="http://sslcert.cc/cgi/cert_down.php"
+certLocation="/tmp/pki"
+CERTFILE=cert.pem
 CERTNAME="Local Root CA"
 
-certLocation="/tmp/pki/"
 mkdir -p $certLocation
-rootCerts=$CERTFILE
-cat <<EOF | base64 -d > $certLocation/$CERTFILE
-MIID/zCCAuegAwIBAgIUXNjylyR7JNg4ZdbIgkba2rEU/tUwDQYJKoZIhvcNAQEL
-...
-EOF
+cert_path=$certLocation/$CERTFILE
+
+curl --silent $CERTURL | grep -v -- "---" | base64 -d > $cert_path
+if [ $? -ne 0 ]; then
+    info "Cannot download SSL certificate.\nPlease contact network administrator"
+    exit 1
+fi
+
+#sed -n "/BEGIN CERTIFICATE/,/END CERTIFICATE/p" test.pem |grep -v -- "---" | base64 -d > $cert_path
+
+if [ x"$1" != x ]; then
+    gui=0
+fi
 install_cert
